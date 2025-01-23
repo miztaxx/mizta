ea56a2b1359ed2bfc3c9ea0ad30addcb5820b665
miztaxx <168346585+miztaxx@users.noreply.github.com>
2025-01-23 15:51:12 +0530

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
-import {generateWAMessageFromContent} from "baileys";
-import {tiktokdl} from '@bochilteam/scraper';
+import { generateWAMessageFromContent } from "baileys";
+import { tiktokdl } from '@bochilteam/scraper';
 
 let tiktok;
 import('@xct007/frieren-scraper')
@@ -9,70 +9,97 @@ import('@xct007/frieren-scraper')
     tiktok = module.tiktok;
   })
   .catch((error) => {
-    console.error('No se pudo importar "@xct007/frieren-scraper".');
+    console.error('Failed to import "@xct007/frieren-scraper".');
   });
 
-const handler = async (m, {conn, text, args, usedPrefix, command}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
-  const tradutor = _translate.plugins.descargas_tiktok
+const handler = async (m, { conn, text, args, usedPrefix, command }) => {
+  if (!text) throw `Please provide a valid TikTok URL! Example: _${usedPrefix + command} https://vt.tiktok.com/ZS6pwFLBM/_`;
 
+  if (!/(?:https:?\/{2})?(?:w{3}|vm|vt|t)?\.?tiktok.com\/([^\s&]+)/gi.test(text)) {
+    throw `Invalid TikTok URL! Example: _${usedPrefix + command} https://vt.tiktok.com/ZS6pwFLBM/_`;
+  }
 
-  if (!text) throw `${tradutor.texto1} _${usedPrefix + command} https://vm.tiktok.com/ZM686Q4ER/_`;
-  if (!/(?:https:?\/{2})?(?:w{3}|vm|vt|t)?\.?tiktok.com\/([^\s&]+)/gi.test(text)) throw `${tradutor.texto2} _${usedPrefix + command} https://vm.tiktok.com/ZM686Q4ER/_`;
-  const texto = `${tradutor.texto3}`;
-  // let buttons = [{ buttonText: { displayText: '♫ 𝙰𝚄𝙳𝙸𝙾 ♫' }, buttonId: `${usedPrefix}tomp3` }]
+  const messageContent = `> 🍄 : Downloading youre tiktok video !`;
+
   try {
-    const aa = {quoted: m, userJid: conn.user.jid};
-    const prep = generateWAMessageFromContent(m.chat, {extendedTextMessage: {text: texto, contextInfo: {externalAdReply: {title: 'ᴛʜᴇ ᴍʏsᴛɪᴄ - ʙᴏᴛ', body: null, thumbnail: imagen1, sourceUrl: 'https://github.com/BrunoSobrino/TheMystic-Bot-MD'}, mentionedJid: [m.sender]}}}, aa);
-    await conn.relayMessage(m.chat, prep.message, {messageId: prep.key.id, mentions: [m.sender]});
+    const aa = { quoted: m, userJid: conn.user.jid };
+    const prep = generateWAMessageFromContent(
+      m.chat,
+      {
+        extendedTextMessage: {
+          text: messageContent,
+          contextInfo: {
+            externalAdReply: {
+              title: 'ᴍɪᴢᴛʏ ʙᴇᴛᴀ 🌙',
+              thumbnail: imagen1,
+              sourceUrl: 'https://mizta-x.com',
+            },
+            mentionedJid: [m.sender],
+          },
+        },
+      },
+      aa
+    );
+
+    await conn.relayMessage(m.chat, prep.message, { messageId: prep.key.id, mentions: [m.sender] });
+
     const dataFn = await conn.getFile(`${global.MyApiRestBaseUrl}/api/tiktokv2?url=${args[0]}&apikey=${global.MyApiRestApikey}`);
-    const desc1n = `${tradutor.texto4[0]} _${usedPrefix}tomp3_ ${tradutor.texto4[1]}`;
-    await conn.sendMessage(m.chat, {video: dataFn.data, caption: desc1n}, {quoted: m});
-  } catch (ee1) {
-  try {
-    //const aa = {quoted: m, userJid: conn.user.jid};
-    //const prep = generateWAMessageFromContent(m.chat, {extendedTextMessage: {text: texto, contextInfo: {externalAdReply: {title: 'ᴛʜᴇ ᴍʏsᴛɪᴄ - ʙᴏᴛ', body: null, thumbnail: imagen1, sourceUrl: 'https://github.com/BrunoSobrino/TheMystic-Bot-MD'}, mentionedJid: [m.sender]}}}, aa);
-    //await conn.relayMessage(m.chat, prep.message, {messageId: prep.key.id, mentions: [m.sender]});
-    const dataF = await tiktok.v1(args[0]);
-    // let desc1 =  `*𝙽𝙸𝙲𝙺𝙽𝙰𝙼𝙴:* ${dataF.nickname || 'Indefinido'}`
-    const desc1 = `${tradutor.texto5[0]} _${usedPrefix}tomp3_ ${tradutor.texto5[1]}`;
-    await conn.sendMessage(m.chat, {video: {url: dataF.play}, caption: desc1}, {quoted: m});
-  } catch (e1) {
-    try {
-      const tTiktok = await tiktokdlF(args[0]);
-      // let desc2 = `🔗 *Url:* ${tTiktok.video}`
-      const desc2 = `${tradutor.texto6[0]} _${usedPrefix}tomp3_ ${tradutor.texto6[1]}`;
-      await conn.sendMessage(m.chat, {video: {url: tTiktok.video}, caption: desc2}, {quoted: m});
-    } catch (e2) {
-        try {
-          const {author: {nickname}, video, description} = await tiktokdl(args[0]);
-          const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd;
-          // let cap = `*𝙽𝙸𝙲𝙺𝙽𝙰𝙼𝙴:* ${nickname || 'Indefinido'}`
-          const cap = `${tradutor.texto8[0]} _${usedPrefix}tomp3_ ${tradutor.texto8[1]}`;
-          await conn.sendMessage(m.chat, {video: {url: url}, caption: cap}, {quoted: m});
-        } catch {
-          throw `${tradutor.texto9}`;
-        }
-      }
-    }
+    const caption = `Here is your video! Use _${usedPrefix}tomp3_ to extract audio if needed.`;
+    await conn.sendMessage(m.chat, { video: dataFn.data, caption }, { quoted: m });
+  } catch (error) {
+    await handleFallbacks(m, conn, args, usedPrefix, command);
   }
 };
+
 handler.command = /^(tiktok|ttdl|tiktokdl|tiktoknowm|tt|ttnowm|tiktokaudio)$/i;
 export default handler;
 
-async function tiktokdlF(url) {
-  if (!/tiktok/.test(url)) return `${tradutor.texto10} _${usedPrefix + command} https://vm.tiktok.com/ZM686Q4ER/_`;
-  const gettoken = await axios.get('https://tikdown.org/id');
-  const $ = cheerio.load(gettoken.data);
-  const token = $('#download-form > input[type=hidden]:nth-child(2)').attr( 'value' );
-  const param = {url: url, _token: token};
-  const {data} = await axios.request('https://tikdown.org/getAjax?', {method: 'post', data: new URLSearchParams(Object.entries(param)), headers: {'content-type': 'application/x-www-form-urlencoded; charset=UTF-8', 'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36'}});
-  const getdata = cheerio.load(data.html);
-  if (data.status) {
-    return {status: true, thumbnail: getdata('img').attr('src'), video: getdata('div.download-links > div:nth-child(1) > a').attr('href'), audio: getdata('div.download-links > div:nth-child(2) > a').attr('href')};
-  } else {
-    return {status: false};
+async function handleFallbacks(m, conn, args, usedPrefix, command) {
+  try {
+    const dataF = await tiktok.v1(args[0]);
+    const caption = `🍄 : Here Is Youre Video\n\n> ꜱɪᴍᴘʟᴇ ɪꜱ ʙᴇᴀᴜᴛʏ 🌙`;
+    await conn.sendMessage(m.chat, { video: { url: dataF.play }, caption }, { quoted: m });
+  } catch (e1) {
+    try {
+      const tTiktok = await tiktokdlF(args[0]);
+      const caption = `Here is your video! Use _${usedPrefix}tomp3_ to extract audio if needed.`;
+      await conn.sendMessage(m.chat, { video: { url: tTiktok.video }, caption }, { quoted: m });
+    } catch (e2) {
+      try {
+        const { author: { nickname }, video } = await tiktokdl(args[0]);
+        const url = video.no_watermark2 || video.no_watermark || `https://tikcdn.net${video.no_watermark_raw}` || video.no_watermark_hd;
+        const caption = `🍄 : Here Is Youre Video\n\n> ꜱɪᴍᴘʟᴇ ɪꜱ ʙᴇᴀᴜᴛʏ 🌙`;
+        await conn.sendMessage(m.chat, { video: { url }, caption }, { quoted: m });
+      } catch {
+        throw `Failed to download the video. Please ensure the URL is correct or try again later.`;
+      }
+    }
   }
+}
+
+async function tiktokdlF(url) {
+  if (!/tiktok/.test(url)) return `Invalid TikTok URL! Example: _${usedPrefix + command} https://vt.tiktok.com/ZS6pwFLBM/_`;
+
+  const response = await axios.get('https://tikdown.org/id');
+  const $ = cheerio.load(response.data);
+  const token = $('#download-form > input[type=hidden]:nth-child(2)').attr('value');
+
+  const params = new URLSearchParams({ url, _token: token });
+  const { data } = await axios.post('https://tikdown.org/getAjax?', params.toString(), {
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36',
+    },
+  });
+
+  if (data.status) {
+    const result = cheerio.load(data.html);
+    return {
+      status: true,
+      thumbnail: result('img').attr('src'),
+      video: result('div.download-links > div:nth-child(1) > a').attr('href'),
+      audio: result('div.download-links > div:nth-child(2) > a').attr('href'),
+    };
+  }
+  return { status: false };
 }
